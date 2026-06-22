d7e600b11a38f0d6b764f5c8000b94086b7b2dfe
Sevolode <dritenglish@gmail.com>
2026-06-22 10:19:44 +0600

    feat: created extension method for valid polindrom

---
+++ task01/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace task01;
+
+public static class StringExtensions
+{
+    static bool IsPalindrome(this string s)
+    {
+        int l = 0; int r = s.Length - 1;
+        if (r < 0) { return false; }
+        while (l < r)
+        {
+            if (s[l] != s[r]) { return false; }
+            do { l++; } while (Char.IsPunctuation(s[l]) || Char.IsWhiteSpace(s[l]));
+            do { r--; } while (Char.IsPunctuation(s[r]) || Char.IsWhiteSpace(s[r]));
+        }
+        return true;
+    }
+}
