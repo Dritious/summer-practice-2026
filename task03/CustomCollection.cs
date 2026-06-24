101c9f8d1db0f79264cd15f0804960954d1259af
Sevolode <dritenglish@gmail.com>
2026-06-24 19:33:54 +0600

    style: dotnet format

--- task03/CustomCollection.cs
+++ task03/CustomCollection.cs
@@ -22,7 +22,7 @@ public class CustomCollection<T> : IEnumerable<T>
     // Шаг 6
     public static IEnumerable<int> GenerateSequence(int start, int count)
     {
-        for (int i = start; i<count; i++)
+        for (int i = start; i < count; i++)
         {
             yield return i;
         }
