cca4772735fffb644875755b28ab79cc6c91e53c
Sevolode <dritenglish@gmail.com>
2026-06-24 18:43:22 +0600

    test: add unit tests

--- task02tests/UnitTest1.cs
+++ task02tests/UnitTest1.cs
@@ -1,10 +1,34 @@
 namespace task02tests;
+using task02;
 
-public class UnitTest1
+public class StudentServiceTests
 {
-    [Fact]
-    public void Test1()
-    {
+    private List<Student> _testStudents;
+    private StudentService _service;
 
+    public StudentServiceTests()
+    {
+        _testStudents = new List<Student>
+        {
+            new() { Name = "Иван", Faculty = "ФИТ", Grades = new List<int> { 5, 4, 5 } },
+            new() { Name = "Анна", Faculty = "ФИТ", Grades = new List<int> { 3, 4, 3 } },
+            new() { Name = "Петр", Faculty = "Экономика", Grades = new List<int> { 5, 5, 5 } }
+        };
+        _service = new StudentService(_testStudents);
+    }
+
+    [Fact]
+    public void GetStudentsByFaculty_ReturnsCorrectStudents()
+    {
+        var result = _service.GetStudentsByFaculty("ФИТ").ToList();
+        Assert.Equal(2, result.Count);
+        Assert.True(result.All(s => s.Faculty == "ФИТ"));
+    }
+
+    [Fact]
+    public void GetFacultyWithHighestAverageGrade_ReturnsCorrectFaculty()
+    {
+        var result = _service.GetFacultyWithHighestAverageGrade();
+        Assert.Equal("Экономика", result);
     }
 }
