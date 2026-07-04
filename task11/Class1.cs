ac9390b0448add3805055ba6088d4fd57a2a6e08
Sevolode <dritenglish@gmail.com>
2026-07-04 15:49:10 +0600

    feat: add class for dynamic compilation

---
+++ task11/Class1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.CSharp;
+using System.CodeDom.Compiler;
+
+public static class DynamicClassCompiler
+{
+    private const string ClassDefinition = @"
+public class Calculator
+{
+    public int Add(int a, int b) => a + b;
+    public int Minus(int a, int b) => a - b;
+    public int Mul(int a, int b) => a * b;
+    public int Div(int a, int b) => a / b;
+}";
+
+    public static dynamic CreateCalculator()
+    {
+        using var provider = new CSharpCodeProvider();
+        var parameters = new CompilerParameters();
+        // подключение базовой библиотеки
+        parameters.ReferencedAssemblies.Add("System.dll");
+
+        var results = provider.CompileAssemblyFromSource(parameters, ClassDefinition);
+
+        var type = results.CompiledAssembly.GetType("Calculator");
+        return Activator.CreateInstance(type);
+    }
+}
+
