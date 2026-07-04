7c1e291af589cec7009110598a18da87fe9dfb02
Sevolode <dritenglish@gmail.com>
2026-07-04 12:16:44 +0600

    feat(test): add dynamic lib logic to console app and remove from unit tests

--- CommandRunner/Program.cs
+++ CommandRunner/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+
+class Program
+{
+    // вспомогательная функция на поиск корня решения
+    private static string FindSolutionRoot()
+    {
+        var directory = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
+        while (directory != null)
+        {
+            if (directory.GetFiles("*.sln").Any())
+            {
+                return directory.FullName;
+            }
+            directory = directory.Parent;
+        }
+        throw new Exception("There is no solution file(.sln)");
+    }
+
+    private static Assembly LoadPlugins()
+    {
+        var solutionDir = FindSolutionRoot();
+        var pluginsDir = Path.Combine(solutionDir, "plugins");
+        var dllPath = Path.Combine(solutionDir, "plugins", "FileSystemCommands.dll");
+
+        // подгружаем все плагины(в том числе интерфейс комманды)
+        foreach (var dll in Directory.GetFiles(pluginsDir, "*.dll"))
+        {
+            Assembly.LoadFrom(dll);
+        }
+
+        return Assembly.LoadFrom(dllPath);
+    }
+
+    // взял пример из тестов
+    public static void Main(string[] args)
+    {
+        var testDir = Path.Combine(Path.GetTempPath(), "TestDir");
+        Directory.CreateDirectory(testDir);
+        File.WriteAllText(Path.Combine(testDir, "test1.txt"), "Hello");
+        File.WriteAllText(Path.Combine(testDir, "test2.txt"), "World");
+
+        var assembly = LoadPlugins();
+        var dirSizeType = assembly.GetType("FileSystemCommands.DirectorySizeCommand");
+        dynamic dirSizeCommand = Activator.CreateInstance(dirSizeType, testDir);
+        dirSizeCommand.Execute();
+
+        var findFilesType = assembly.GetType("FileSystemCommands.FindFilesCommand");
+        dynamic findFilesCommand = Activator.CreateInstance(findFilesType, testDir, "*.txt");
+        findFilesCommand.Execute();
+    }
+}
--- task08tests/UnitTest1.cs
+++ task08tests/UnitTest1.cs
@@ -1,31 +1,9 @@
-using System.Reflection;
+using FileSystemCommands;
 
 namespace task08tests;
 
-
 public class FileSystemCommandsTests
 {
-    // вспомогательная функция на поиск корня решения
-    private static string FindSolutionRoot()
-    {
-        var directory = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
-        while (directory != null)
-        {
-            if (directory.GetFiles("*.sln").Any())
-            {
-                return directory.FullName;
-            }
-            directory = directory.Parent;
-        }
-        throw new Exception("There is no solution file(.sln)");
-    }
-
-    private static Assembly LoadPlugins()
-    {
-        var solutionDir = FindSolutionRoot();
-        var dllPath = Path.Combine(solutionDir, "plugins", "FileSystemCommands.dll");
-        return Assembly.LoadFrom(dllPath);
-    }
 
     [Fact]
     public void DirectorySizeCommand_ShouldCalculateSize()
@@ -35,9 +13,7 @@ public class FileSystemCommandsTests
         File.WriteAllText(Path.Combine(testDir, "test1.txt"), "Hello");
         File.WriteAllText(Path.Combine(testDir, "test2.txt"), "World");
 
-        var assembly = LoadPlugins();
-        var type = assembly.GetType("DirectorySizeCommand");
-        dynamic command = Activator.CreateInstance(type, testDir);
+        var command = new DirectorySizeCommand(testDir);
 
         var output = new StringWriter();
         Console.SetOut(output);
@@ -57,17 +33,16 @@ public class FileSystemCommandsTests
         File.WriteAllText(Path.Combine(testDir, "file1.txt"), "Text");
         File.WriteAllText(Path.Combine(testDir, "file2.log"), "Log");
 
-        var assembly = LoadPlugins();
-        var type = assembly.GetType("FindFilesCommand");
-        dynamic command = Activator.CreateInstance(type, testDir, "*.txt");
+        var command = new FindFilesCommand(testDir, "*.txt");
 
         var sw = new StringWriter();
         Console.SetOut(sw);
+
         command.Execute();
+
         var output = sw.ToString();
         Console.SetOut(Console.Out);
-
-        var lines = output.Split("\n"); 
+        var lines = output.Split("\n", StringSplitOptions.RemoveEmptyEntries); 
         Assert.Single(lines);
         Assert.Contains("file1.txt", lines[0]);
 
