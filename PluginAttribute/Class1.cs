3c011600cb5b6604b641cbd29f9076f0ea4eee35
Sevolode <dritenglish@gmail.com>
2026-07-04 14:41:31 +0600

    feat: add Plugin attribute

---
+++ PluginAttribute/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace PluginAttribute;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+public class PluginLoadAttribute : Attribute
+{
+    public string Name { get; }
+    public string[] Dependencies { get; }
+
+    public PluginLoadAttribute(string name, params string[] dependencies)
+    {
+        Name = name;
+        Dependencies = dependencies;
+    }
+}
