7aaf2e6b730d51ad6d13ccedc93c78d1c5723afa
Sevolode <dritenglish@gmail.com>
2026-07-04 12:17:52 +0600

    fix: remove regex logic from FileSystemObject and add standart wildcard

--- FileSystemCommands/FileSystemCommands.cs
+++ FileSystemCommands/FileSystemCommands.cs
@@ -1,7 +1,6 @@
 ﻿namespace FileSystemCommands;
 
 using CommandLib;
-using System.Text.RegularExpressions;
 
 public class DirectorySizeCommand: ICommand
 {
@@ -50,32 +49,13 @@ public class FindFilesCommand: ICommand
 
     public void Execute()
     {
-        var regex = new Regex(_pattern);
-        int foundCount = FindFilesRecursive(_directoryPath, regex);
-    }
-    private int FindFilesRecursive(string path, Regex regex)
-    {
-        int count = 0;
-        string[] files = Directory.GetFiles(path);
-        foreach (string file in files)
+        var files = Directory.GetFiles(_directoryPath, _pattern, SearchOption.AllDirectories);
+
+        foreach (var file in files)
         {
-            string fileName = Path.GetFileName(file);
-            if (regex.IsMatch(fileName))
-            {
-                Console.WriteLine($"Find: {file}");
-                count++;
-            }
+            Console.WriteLine($"Found: {file}");
         }
 
-        string[] directories = Directory.GetDirectories(path);
-        foreach (string directory in directories)
-        {
-            count += FindFilesRecursive(directory, regex);
-
-        }
-   
-
-        return count;
     }
 
 }
