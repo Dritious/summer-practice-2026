50654e0d2dcf36d624811b4b6fa96207cd4f63a8
Sevolode <dritenglish@gmail.com>
2026-07-13 06:37:41 +0600

    fix: add null exeptions

--- task03/CustomCollection.cs
+++ task03/CustomCollection.cs
@@ -6,7 +6,11 @@ public class CustomCollection<T> : IEnumerable<T>
 {
     private readonly List<T> _items = new();
 
-    public void Add(T item) => _items.Add(item);
+    public void Add(T item)
+    {
+        if (item == null) throw new ArgumentNullException(nameof(item));
+        _items.Add(item);
+    }
     public IEnumerator<T> GetEnumerator() => _items.GetEnumerator();
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
@@ -31,6 +35,9 @@ public class CustomCollection<T> : IEnumerable<T>
     // Шаг 7
     public IEnumerable<T> FilterAndSort(Func<T, bool> predicate, Func<T, IComparable> keySelector)
     {
+        if (predicate == null) throw new ArgumentNullException(nameof(predicate));
+        if (keySelector == null) throw new ArgumentNullException(nameof(keySelector));
+
         return _items.Where(predicate).OrderBy(keySelector);
     }
 }
