9a216cac38e54a42b6fe9078564e4f973b53179c
Sevolode <dritenglish@gmail.com>
2026-07-13 08:21:29 +0600

    test: add unit tests

---
+++ task10tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿namespace task10tests;
+
+using task10;
+
+public class PluginLoaderTests
+{
+    [Fact]
+    public void LoadPlugins_ThrowsArgumentNullException_WhenPathIsNull()
+    {
+        var loader = new PluginLoader();
+
+        Assert.Throws<ArgumentNullException>(() => loader.LoadPlugins(null!));
+    }
+
+    [Fact]
+    public void LoadPlugins_ThrowsDirectoryNotFoundException_WhenPathDoesNotExist()
+    {
+        var loader = new PluginLoader();
+        var fakePath = Path.Combine(Path.GetTempPath(), "hfdjfhskdfhdjshfdkjshfdjshfdskjhfdksj");
+
+        Assert.Throws<DirectoryNotFoundException>(() => loader.LoadPlugins(fakePath));
+    }
+}
