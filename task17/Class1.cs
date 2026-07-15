af9aafc1334dc7bb692fd4c2a9fdf6ee609db4de
Sevolode <dritenglish@gmail.com>
2026-07-15 08:04:47 +0600

    feat: add ServerThread and commands for it

--- task17/Class1.cs
+++ task17/Class1.cs
@@ -1,6 +1,135 @@
 ﻿namespace task17;
 
-public class Class1
-{
+using System.Collections.Concurrent;
 
+public interface ICommand
+{
+    void Execute();
+}
+
+public class ServerThread
+{
+    private readonly BlockingCollection<ICommand> _queue = new BlockingCollection<ICommand>();
+    private readonly Thread _thread;
+    private readonly Action<ICommand, Exception> _exceptionHandler;
+
+    // делегат, исполняющий комманды 
+    private Action? _behavior;
+
+    public int ManagedThreadId => _thread.ManagedThreadId;
+
+    public ServerThread(Action<ICommand, Exception>? exceptionHandler = null)
+    {
+        // добавляет имя команды в начало сообщения, и пробрасывает дальше
+        _exceptionHandler = exceptionHandler ?? ((cmd, ex) =>
+        {
+            throw new Exception($"[{cmd.GetType().Name}]: {ex.Message}", ex);
+        });
+        _behavior = DefaultBehavior;
+        _thread = new Thread(Run);
+    }
+
+    public void Start() => _thread.Start();
+
+    public void Join(int millisecondsTimeout = Timeout.Infinite) => _thread.Join(millisecondsTimeout);
+
+    public void Enqueue(ICommand cmd)
+    {
+        try
+        {
+            _queue.Add(cmd);
+        }
+        catch (InvalidOperationException ex)
+        {
+            // ловим системную ошибку закрытой коллекции 
+            throw new InvalidOperationException(
+                $"[SoftStop in progress] Cannot enqueue command '{cmd.GetType().Name}'. The queue is closed.",
+                ex
+            );
+        }
+    }
+
+    private void Run()
+    {
+        while (_behavior != null)
+        {
+            _behavior();
+        }
+    }
+
+    private void DefaultBehavior()
+    {
+        if (_queue.TryTake(out var cmd, Timeout.Infinite))
+        {
+            ExecuteCommand(cmd);
+        }
+    }
+
+    internal void HardStop()
+    {
+        _behavior = null;
+    }
+
+    internal void SoftStop()
+    {
+        // закрываем очередь при остановке
+        _queue.CompleteAdding();
+        _behavior = () =>
+        {
+            if (!_queue.IsCompleted)
+            {
+                if (_queue.TryTake(out var cmd))
+                {
+                    ExecuteCommand(cmd);
+                }
+            }
+            else
+            {
+                _behavior = null;
+            }
+        };
+    }
+
+    internal void ExecuteCommand(ICommand cmd)
+    {
+        try
+        {
+            cmd.Execute();
+        }
+        catch (Exception ex)
+        {
+            _exceptionHandler(cmd, ex);
+        }
+    }
+}
+
+
+public class HardStopCommand : ICommand
+{
+    private readonly ServerThread _serverThread;
+    public HardStopCommand(ServerThread serverThread) => _serverThread = serverThread;
+
+    public void Execute()
+    {
+        if (Thread.CurrentThread.ManagedThreadId != _serverThread.ManagedThreadId)
+        {
+            throw new InvalidOperationException("Command must execute in server thread.");
+        }
+        _serverThread.HardStop();
+    }
+}
+
+public class SoftStopCommand : ICommand
+{
+    private readonly ServerThread _serverThread;
+    public SoftStopCommand(ServerThread serverThread) => _serverThread = serverThread;
+
+    public void Execute()
+    {
+        if (Thread.CurrentThread.ManagedThreadId != _serverThread.ManagedThreadId)
+        {
+            throw new InvalidOperationException("Command must execute in server thread.");
+        }
+        _serverThread.SoftStop();
+    }
 }
